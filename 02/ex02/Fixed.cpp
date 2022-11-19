46a92f24ae24f761df304f3617c7d33cae60f1f5
Keeghaan <lemaildejo@gmail.com>
2022-11-19 15:09:40 +0100

    02 02 en cours

--- 02/ex02/Fixed.cpp
+++ 02/ex02/Fixed.cpp
@@ -2,30 +2,30 @@
 
 Fixed::Fixed(void): _n(0)
 {
-	std::cout << "Default constructor called" << std::endl;
+//	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &copy)
 {
-	std::cout << "Copy constructor called" << std::endl;
+//	std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
 }
 
 Fixed::Fixed(const float nf)
 {
-	std::cout << "Float constructor called" << std::endl;
+//	std::cout << "Float constructor called" << std::endl;
 	this->_n = roundf(nf * (1 << this->_bits));
 }
 
 Fixed::Fixed(const int n)
 {
-	std::cout << "Int constructor called" << std::endl;
+//	std::cout << "Int constructor called" << std::endl;
 	this->_n = n << this->_bits;
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructor called" << std::endl;
+//	std::cout << "Destructor called" << std::endl;
 }
 
 int	Fixed::getRawBits(void) const
@@ -48,11 +48,30 @@ float	Fixed::toFloat(void) const
 	return (float(this->_n) / (1 << this->_bits));
 }
 
+int	Fixed::min(Fixed n1, Fixed n2)
+{
+
+}
+
+int	Fixed::max(Fixed n1, Fixed n2)
+{
+
+} 
+
+int	Fixed::min(const Fixed n1, const Fixed n2)
+{
+
+}
+
+int	Fixed::max(const Fixed n1, const Fixed n2)
+{
+
+} 
 		//ASSIGNMENT =
 
 Fixed	&Fixed::operator=(const Fixed &rhs)
 {
-	std::cout << "Copy assignment operator called." << std::endl;
+//	std::cout << "Copy assignment operator called." << std::endl;
 	this->_n = rhs.getRawBits();
 	return (*this);
 }
