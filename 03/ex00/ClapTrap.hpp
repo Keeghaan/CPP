50d67d1b4b2780f26e6d420456c8b21a4121fac3
Johanna COURTOIS <jcourtoi@e1r4p12.clusters.42paris.fr>
2022-11-23 17:53:25 +0100

    03

---
+++ 03/ex00/ClapTrap.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcourtoi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 17:28:24 by jcourtoi          #+#    #+#             */
+/*   Updated: 2022/11/23 17:30:34 by jcourtoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+# define DEBUG 1
+
+class	ClapTrap
+{
+	public:
+
+	private:
+		std::string	_name;
+		int			_pv;
+		int			_pe;
+
+}
+
+#endif
