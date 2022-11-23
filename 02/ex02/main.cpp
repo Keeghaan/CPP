18eebfacbf0cfe70c23e347c224f3121d99ea37d
Johanna COURTOIS <jcourtoi@e1r4p12.clusters.42paris.fr>
2022-11-23 12:23:46 +0100

    02

--- 02/ex02/main.cpp
+++ 02/ex02/main.cpp
@@ -15,22 +15,29 @@ int	main(void)
 		std::cout << Fixed::max( a, b ) << std::endl;
 	}
 	std::cout << std::endl;
-/*	{
+	{
 		Fixed const b( Fixed( 5.05f ) * Fixed (2) );
 		Fixed		a;
-		Fixed const	b(Fixed(2.05f) + Fixed(2));
-		Fixed		c(2.2f);
-		Fixed		d(5.0f);
-		c.setRawBits(2);
-		a.setRawBits(5);
-		std::cout << "a._n = " << a.getRawBits() << std::endl;
+		Fixed const	c(Fixed(2.05f) + Fixed(2));
+		Fixed		d(2.2f);
+		Fixed		f(5.0f);
+
+		std::cout << "a = " << a << std::endl;
+		std::cout << "b = " << b << std::endl;
+		std::cout << "c = " << c << std::endl;
+		std::cout << "d = " << d << std::endl;
+		std::cout << "f = " << f << std::endl;
 		std::cout << "++a = " << ++a << std::endl;
+		std::cout << "a = " << a << std::endl;
 		std::cout << "a + a = " << a + a << std::endl;
-		std::cout << "--a = " << --a << std::endl;
+		std::cout << "--f = " << --f << std::endl;
+		std::cout << "f = " << f << std::endl;
 		std::cout << "a = " << a << std::endl;
 		std::cout << b << std::endl;
+		if (a < d)
+			std::cout << "a < d" << std::endl;
 		std::cout << "min b ? a ? " << Fixed::min(b, a) << std::endl;
 		std::cout << "c * d = " << c * d << std::endl;
-	}*/
+	}
 	return (0);
 }
