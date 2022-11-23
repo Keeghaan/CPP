29d2c3c3d4c419142232c4d43c22d941497e874b
Johanna COURTOIS <jcourtoi@e1r4p12.clusters.42paris.fr>
2022-11-23 17:23:55 +0100

    02ex03

--- 02/ex03/bsp.cpp
+++ 02/ex03/bsp.cpp
@@ -6,28 +6,50 @@
 /*   By: jcourtoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 12:59:01 by jcourtoi          #+#    #+#             */
-/*   Updated: 2022/11/23 16:03:10 by jcourtoi         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:22:00 by jcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+#include <cstdio>
 
-float	area(const Point a, const Point b, const Point c)
+bool	is_neg(const Point a, const Point b, const Point c)
+{
+	if (a.getX() < 0 || a.getY() < 0 || b.getX() < 0 ||  b.getY() < 0
+		|| c.getX() < 0 || c.getY() < 0)
+		return (true);
+	return (false);
+}
+
+Fixed	area(const Point a, const Point b, const Point c)
 {
 	Point	tmp(0.5, 1.5);
 	Fixed	result;
 
-	result = (tmp.getX() * (b.getX() - a.getX()) * (c.getY() - a.getY()) - (c.getX() - a.getX()) * (b.getY() - a.getY()));
-	return (result.toFloat());
+	if (!is_neg(a, b, c))
+		result = (tmp.getX() * (b.getX() - a.getX()) * (c.getY() - a.getY()) -
+			(c.getX() - a.getX()) * (b.getY() - a.getY()));
+	else
+	{
+		result = a.getX() * (b.getY() - c.getY()) +  b.getX() *
+			(a.getY() - c.getY()) + c.getX() * (b.getY() - c.getY());
+		std::abs(result.toFloat());
+	}
+	return (result);
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
+	std::cout << area(a, b, c) << std::endl;
+	std::cout << area(a, b, point) << std::endl;
+	std::cout << area(b, c, point) << std::endl;
+	std::cout << area(a, c, point) << std::endl;
 	std::cout << area(a, b, point) + area(b, c, point) + area(a, c, point) << std::endl;
-	if (area(a, b, point) + area(b, c, point) + area(a, c, point) == 0)
-		return (false);
-	if (area(a, b, point) + area(b , c, point)  + area(a, c, point) > 0)
-		return (false);
-	
+
+	if ((area(a, b, point) + area(b, c, point) + area(a, c, point)) > 0)//> area(a, b, c))
+		return (printf("ici\n"), false);
+	if (area(a, b, point) == 0 || area(b, c, point) == 0
+		|| area(a, c, point) == 0)
+		return (printf("la\n"), false);
 	return (true);
 }
--- 02/ex03/main.cpp
+++ 02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcourtoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 13:42:08 by jcourtoi          #+#    #+#             */
-/*   Updated: 2022/11/23 16:15:21 by jcourtoi         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:20:54 by jcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 int	main(void)
 {
+	{
 		Point	a(4.0, 3.0), b(2.26, 1.15), c(6.74, 0.67), point(3.56, 1.59);
 		Point	d;
 		Point	f(4.58, 0.90);
 		Point	e(6.3, 2.61);
 		
-		d = a;
+		d = c;
 		std::cout << "Point in the triangle : " << std::endl;
 		if (bsp(a, b, c, point))
 			std::cout << "The Point is in the Triangle !" << std::endl;
@@ -30,15 +31,44 @@ int	main(void)
 			std::cout << "The Point is in the Triangle !" << std::endl;
 		else
 			std::cout << "The Point is not in the Triangle !" << std::endl;
-		std::cout << std::endl << "Point on the triangle (so not in)" << std::endl;
+		std::cout << std::endl << "Point on the triangle" << std::endl;
 		if (bsp(a, b, c, f))
 			std::cout << "The Point is in the Triangle !" << std::endl;
 		else
 			std::cout << "The Point is not in the Triangle !" << std::endl;
-		std::cout << std::endl << "Point on the triangle (so not in)" << std::endl;
+		std::cout << std::endl << "Point on the triangle" << std::endl;
 		if (bsp(a, b, c, d))
 			std::cout << "The Point is in the Triangle !" << std::endl;
 		else
 			std::cout << "The Point is not in the Triangle !" << std::endl;
+	}
+	{
+		Point	a(-4.50, -0.81), b(-5.93, -1.78), c(-3.73, -1.48), point(-4.82, -1.36);
+		Point	d;
+		Point	f(-4.77, -3.53);
+		Point	e(6.3, 2.61);
+		
+		d = c;
+		std::cout << "Point in the triangle : " << std::endl;
+		if (bsp(a, b, c, point))
+			std::cout << "The Point is in the Triangle !" << std::endl;
+		else
+			std::cout << "The Point is not in the Triangle !" << std::endl;
+		std::cout << std::endl << "Point not in the triangle : " << std::endl;
+		if (bsp(a, b, c, e))
+			std::cout << "The Point is in the Triangle !" << std::endl;
+		else
+			std::cout << "The Point is not in the Triangle !" << std::endl;
+		std::cout << std::endl << "Point on the triangle" << std::endl;
+		if (bsp(a, b, c, f))
+			std::cout << "The Point is in the Triangle !" << std::endl;
+		else
+			std::cout << "The Point is not in the Triangle !" << std::endl;
+		std::cout << std::endl << "Point on the triangle" << std::endl;
+		if (bsp(a, b, c, d))
+			std::cout << "The Point is in the Triangle !" << std::endl;
+		else
+			std::cout << "The Point is not in the Triangle !" << std::endl;
+	}
 	return (0);
 }
