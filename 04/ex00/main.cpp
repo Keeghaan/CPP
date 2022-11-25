4a411ba8ed1fd165b6fc5304ec48cbd7eafbd839
Johanna COURTOIS <jcourtoi@e1r10p15.clusters.42paris.fr>
2022-11-25 16:56:52 +0100

    0400

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -6,25 +6,44 @@
 /*   By: jcourtoi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 21:44:25 by jcourtoi          #+#    #+#             */
-/*   Updated: 2022/11/25 15:55:15 by jcourtoi         ###   ########.fr       */
+/*   Updated: 2022/11/25 16:56:41 by jcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete i;
-	delete j;
-	delete meta;
+	{
+		const WrongAnimal* meta = new WrongAnimal();
+		const Animal* j = new Dog();
+		const WrongAnimal* i = new WrongCat();
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); //will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+	
+		delete i;
+		delete j;
+		delete meta;
+	}
+	std::cout << std::endl;
+	{
+		const Animal* meta = new Animal();
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		std::cout << j->getType() << " " << std::endl;
+		std::cout << i->getType() << " " << std::endl;
+		i->makeSound(); //will output the cat sound!
+		j->makeSound();
+		meta->makeSound();
+	
+		delete i;
+		delete j;
+		delete meta;
+	}
 	return 0;
 }
