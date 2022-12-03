d3a71eee60f09d6cf5a2ae1ee6530c0c5eecdf24
Keeghaan <lemaildejo@gmail.com>
2022-12-03 19:58:52 +0100

    07ex00

--- 07/ex00/whatever.hpp
+++ 07/ex00/whatever.hpp
@@ -13,11 +13,28 @@
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
+# include <iostream>
+
 template <typename T>
-const T	&max(const T &x, const T &y)
+const	T	&max(const T &x, const T &y)
 {
-	
+	return (x >= y ? x : y);
 }
 
+template <typename T>
+const T	&min(const T &x, const T &y)
+{
+	return (x < y ? x : y);
+}
+
+template <typename T>
+void	swap(T &x, T &y)
+{
+	T	tmp;
+
+	tmp = x;
+	x = y;
+	y = tmp;
+}
 
 #endif
